3af7f74d27d223b1dd3f38c6d42d612b72f40603
ted <tedleiming@gmail.com>
2020-01-03 10:41:27 +0800

    登陆

--- pages/page_lm/register/verifyLogin.vue
+++ pages/page_lm/register/verifyLogin.vue
@@ -3,22 +3,25 @@
 		<image class="vlImg" src="../../../static/image/logo.png" mode=""></image>
 		<view class="vlViIn fx">
 			<text class="vlIntext">手机号</text>
-			<input class="vlInput " type="text" :value="phone" placeholder="请输入手机号码" />
+			<input class="vlInput " type="text" v-model="phone" placeholder="请输入手机号码" />
 			<!-- focus -->
+			<text v-show="phone" class="iconfont" @click="topClear">&#xe600;</text>
 		</view>
 		<view class="vlViIn fx inMagin">
 			<text class="vlIntext">验证码</text>
-			<input class="vlInput" type="text" :value="phone" placeholder="请输入手机号码"/>
+			<input class="vlInput" type="text" v-model="verifyCode" placeholder="请输入手机号码"/>
 			<view class="vlInYzm">
-				<text>获取验证码</text>
+				<text @click="topGetVerify()">获取验证码</text>
 			</view>
 		</view>
 		<view class="vlRa fx">
-			<video class="vlRadio"></video>
-			<text>我已经阅读并同意</text>
-			<navigator url="">服务协议</navigator>
+			<view class="vlRadio" @click="topAgree" :class="isAgree?'bgcolor':''">
+				<text v-show="isAgree" class="iconfont">&#xe63e;</text>
+			</view>
+			<text style="margin-left: 18upx;">我已经阅读并同意</text>
+			<navigator class="vlRaNav" url="">服务协议</navigator>
 			<text>、</text>
-			<navigator url="">隐私保护政策</navigator>
+			<navigator class="vlRaNav" url="">隐私保护政策</navigator>
 		</view>
 	</view>
 </template>
@@ -27,11 +30,26 @@
 export default {
 	data() {
 		return {
-			phone:''
+			phone:'',
+			verifyCode:'',
+			isAgree:false
 		}
 	},
 	methods: {
-
+		//清除手机号
+		topClear(){
+			this.phone=''
+		},
+		
+		//同意协议
+		topAgree(){
+			this.isAgree = !this.isAgree;
+		},
+		
+		//获取验证码
+		topGetVerify(){
+			console.log(this.phone)
+		}
 	},
 	
 	onNavigationBarButtonTap(e) {
@@ -61,6 +79,9 @@ export default {
 			height: 100%;
 			margin-left: 32upx;
 		}
+		.iconfont{
+			
+		}
 		.vlInYzm{
 			width:200upx;
 			height:80upx;
@@ -80,15 +101,28 @@ export default {
 	}
 	.vlRa{
 		width: 630upx;
-		margin:10upx auto;
+		margin:20upx auto;
 		font-size: 24upx;
 		color: #333333;
+		align-items: center;
 		.vlRadio{
 			width: 30upx;
 			height: 30upx;
 			border:1upx solid rgba(112,112,112,1);
 			border-radius: 50%;
-			background: #fff;
+			text-align: center;
+			line-height: 30upx;
+			.iconfont{
+				font-size: 30upx;
+			}
+		}
+		.vlRaNav{
+			color: #2AB0EA;
+		}
+		.bgcolor{
+			background: #FD5C0E;
+			border-color: #FD5C0E;
+			color: #fff;
 		}
 	}
 }

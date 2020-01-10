070b04983f994fe2c5a344b1021eb79505e9a9a9
lzj <773114277@qq.com>
2020-01-10 13:38:57 +0800

    首页整体布局完成

--- components/components_lm/listPage/tableBer.vue
+++ components/components_lm/listPage/tableBer.vue
@@ -1,40 +1,43 @@
 <template>
 	<view class="tb_app fx">
-		<view class="tb_option fx" v-for="(item,i) of selectList" :key="i" @tap="topSel(i)">
+		<view class="tb_option fx active" v-for="(item,i) of selectList" :key="i" @tap="topSel(i)">
 			<text>{{item}}</text>
-			<view class="tb_select" v-if="i===(!settingNum&&settingNum!==0?0:settingNum)"></view>
+			<view class="tb_select" v-if="i===n"></view>
 		</view>
 	</view>
 </template>
 
 <script>
 export default {
-	props:['settingNum'],
+	props:['settingNum', 'onSelect'],
 	data() {
 		return {
+			n: 0,
 			selectList:['综合','课程','学校','教师','资讯'],
 		}
+	},
+	created() {
+		this.n = this.settingNum;
 	},
 	methods: {
 		//点击时
 		topSel(num){
+			this.n = num;
 			this.$emit('onSelect',num);
-		},
-		
+		}
 	}
 }
 </script>
 
 <style scoped lang="scss">
 .tb_app{
-	width: 100%;
-	height: 64rpx;
+	width: 100vw;
+	height: 32px;
 	background-color: $col-main;
 	justify-content: space-between;
 	.tb_option{
 		height: 54rpx;
 		padding:0 30rpx;
-		font-size: 32rpx;
 		color: $col-fff;
 		flex-direction: column;
 		justify-content: space-between;
---
+++ components/components_lzj/index/main/main.vue
@@ -0,0 +1,82 @@
+<template>
+	<view class="index_main fx">
+		<view class="information fx">
+			<text class="information_one">动态资讯</text>
+			<text class="information_two fx fxCenter">热门</text>
+		</view>
+		<image class="section_image" :src="informationImage.pic" mode=""></image>
+		<!-- 热门课程 -->
+		<uHotCurriclum :isUpdateData.sync="isUpdateData"></uHotCurriclum>
+		<!-- 热门学校 -->
+		<uHotSchool :isUpdateData.sync="isUpdateData"></uHotSchool>
+		<!-- 热门课程推荐 -->
+		<uCategory :isUpdateData.sync="isUpdateData"></uCategory>
+	</view>
+</template>
+
+<script>
+	import uCategory from '../hotTopic/category.vue'
+	import uHotCurriclum from '../hotTopic/hotCurriclum.vue'
+	import uHotSchool from '../hotTopic/hotSchool.vue'
+	export default {
+		components: {
+			uCategory,
+			uHotCurriclum,
+			uHotSchool
+		},
+		props: ['isUpdateData'],
+		data() {
+			return {
+				informationImage: {},
+			}
+		},
+		created() {
+			this.getIformation();
+		},
+		methods: {
+			/**
+			 * 获取热门资讯广告位
+			 */
+			async getIformation() {
+				this.informationImage = (await this.getAdvertisingData(5))[0];
+			}
+		}
+	}
+</script>
+
+<style scoped lang="scss">
+	.index_main{
+		flex-direction: column;
+		align-items: center;
+		.information {
+			align-items: center;
+			width: 690rpx;
+			height: 60rpx;
+			margin: 28rpx 0 40rpx;
+			background: $col-fff;
+			border-radius: 50rpx;
+		
+			.information_one {
+				margin: 0 24rpx;
+				font-size: 28rpx;
+				font-weight: 700;
+				color: $col-333;
+			}
+		
+			.information_two {
+				width: 48rpx;
+				height: 28rpx;
+				font-size: 20rpx;
+				background-color: rgba(253, 129, 9, 0.2);
+				color: $col-main;
+				border-radius: 4rpx;
+			}
+		}
+		
+		.section_image {
+			width: 690rpx;
+			height: 152rpx;
+		}
+	}
+	
+</style>

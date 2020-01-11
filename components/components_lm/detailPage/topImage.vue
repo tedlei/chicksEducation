2de0f37acc56a0973e9542f1f203a67643853d80
ted <tedleiming@gmail.com>
2020-01-11 17:22:42 +0800

    merge

--- components/components_lm/detailPage/topImage.vue
+++ components/components_lm/detailPage/topImage.vue
@@ -1,5 +1,6 @@
 <template>
-	<view class="">
+	<view class="ti_app">
+		<image :src="require('../../../static/image/default.png')" mode=""></image>
 	</view>
 </template>
 
@@ -16,4 +17,10 @@ export default {
 </script>
 
 <style scoped lang="scss">
+.ti_app{
+	width: 100%;
+	image{
+		width: 100%;
+	}
+}
 </style>
